5de939c1f0dfe9e16e3082dd341629f2fedc8dc0
nathanzanin99 <nathanzanin99@gmail.com>
2025-12-28 12:51:36 -0300

    Add files via upload

---
+++ desafio5.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv("C:\\Users\\natha\\OneDrive\\Desktop\\codigos\\curso santander\\usuarios.csv")
+user_ids = df['ID'].tolist()
+
+
+def get_user(id):
+    if id <= len(user_ids):
+        return user_ids[id-1]
+    else:
+        None
+
+users = df.to_dict(orient='records')
+tabela = pd.DataFrame(users)
+
+
+def analise_de_credito():
+    tabela.insert(loc=6,column="Status", value=np.where(tabela["Saldo"]>=1000,"Concedido","Negado"))
+
+
+
+analise_de_credito()
+print(tabela)
+
+tabela.to_csv("usuarios_novo.csv", index=False)
