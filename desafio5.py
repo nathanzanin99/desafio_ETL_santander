a793486e6c85f0dc37eb574ab91c50c02796152f
nathanzanin99 <nathanzanin99@gmail.com>
2025-12-28 12:57:18 -0300

    Refactor credit analysis code in desafio5.py

--- desafio5.py
+++ desafio5.py
@@ -3,19 +3,14 @@ import numpy as np
 df = pd.read_csv("usuarios.csv")
 user_ids = df['ID'].tolist()
 
-
-users = df.to_dict(orient='records')
 tabela = pd.DataFrame(users)
-
-
 def analise_de_credito():
     tabela.insert(loc=6,column="Status", value=np.where(tabela["Saldo"]>=1000,"Concedido","Negado"))
-
-
 
 analise_de_credito()
 print(tabela)
 
 
 tabela.to_csv("usuarios_novo.csv", index=False)
+
 
